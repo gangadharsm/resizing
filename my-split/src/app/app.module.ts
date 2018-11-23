b331153e8acf5d3658f4a9efcea46597ecf2a91c
Gangadhara <gangadhara@bootminds.com>
2018-11-23 18:30:46 +0530

    app created

---
+++ my-split/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AngularSplitModule } from 'angular-split';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularSplitModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
